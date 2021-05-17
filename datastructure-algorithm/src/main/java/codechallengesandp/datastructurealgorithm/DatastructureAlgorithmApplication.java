7d8a40085a60b0d7c17a266259933e4856f5df4f
sandpgaikwad2013 <63579430+sandpgaikwad2013@users.noreply.github.com>
2021-05-17 10:41:04 +0530

    start
    started with new project

---
+++ datastructure-algorithm/src/main/java/codechallengesandp/datastructurealgorithm/DatastructureAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package codechallengesandp.datastructurealgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastructureAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastructureAlgorithmApplication.class, args);
+	}
+
+}
---
+++ datastructure-algorithm/src/test/java/codechallengesandp/datastructurealgorithm/DatastructureAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package codechallengesandp.datastructurealgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastructureAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
